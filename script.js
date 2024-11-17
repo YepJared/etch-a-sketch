d29f68eff1e3a402fd430b19b8f73fa11b1b7233
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-17 16:49:45 -0500

    Style grid to be able to resize with window
    Fix script bug where an extra '.' was being added to the css classList
    Add flex to body to center grid on page
    Add 1 to 1 aspect ratio on the container and grid squres to ensure grid is always a square made up of squares

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const gridContainer = document.querySelector(".container");
 function makeSquares() {
     for (let length = 0; length < 16; length++) {
         const row = document.createElement("div");
-        row.classList.add(".grid-row");
+        row.classList.add("grid-row");
         for (let width = 0; width < 16; width++) {
             const square = document.createElement("div");
-            square.classList.add(".grid-square");
+            square.classList.add("grid-square");
             row.appendChild(square);
         }
         gridContainer.appendChild(row);
