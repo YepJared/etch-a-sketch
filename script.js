04103a3042e6cc36b6d7ddeecc49d4f0f526fc35
YepJared <97353828+YepJared@users.noreply.github.com>
2024-11-17 16:58:19 -0500

    Add effect to color square when hovered
    Refactor grid square production to its own function for readability
    Add mouseover event for making squares red when hovered

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const gridContainer = document.querySelector(".container");
 
+function createGridSquare() {
+    const square = document.createElement("div");
+    square.classList.add("grid-square");
+    square.addEventListener("mouseover", () => { 
+        square.style.backgroundColor = "red";
+    });
+    return square;
+}
+
 function makeSquares() {
     for (let length = 0; length < 16; length++) {
         const row = document.createElement("div");
         row.classList.add("grid-row");
         for (let width = 0; width < 16; width++) {
-            const square = document.createElement("div");
-            square.classList.add("grid-square");
+            const square = createGridSquare();
             row.appendChild(square);
         }
         gridContainer.appendChild(row);
